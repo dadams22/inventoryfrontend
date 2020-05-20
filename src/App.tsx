ffc1fce020fbdfaefffa2c80fdca10863dbe5d71
chriseckart <christopher.eckart@gmail.com>
2020-05-20 14:28:37 -0400

    - npm is our package manager, uninstalled yarn - Installed Ant Design, comes with built in TypeScript definitions, so don't install @types/antd - Installed Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button>Hello</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
