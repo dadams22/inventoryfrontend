537352180b69c0afd2c8659778dfc746178f6db4
David Adams <dadams2212@gmail.com>
2020-05-31 19:05:15 -0400

    Abstract the creation of Menu Items in NavigationBar.tsx

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Menu } from "antd";
+import { Link } from "react-router-dom";
 
 function NavigationBar() {
+    const tabs = [
+        { name: 'Inventory', path: '/inventory' },
+        { name: 'Scales', path: 'scales'}
+    ];
+
     return (
         <Menu mode={'horizontal'}>
-            <Menu.Item key={'inventory'}>Inventory</Menu.Item>
-            <Menu.Item key={'scales'}>Scales</Menu.Item>
+            {tabs.map(tab => (
+                <Menu.Item key={tab.name}>
+                    <Link to={tab.path}>{tab.name}</Link>
+                </Menu.Item>
+            ))}
         </Menu>
     )
 }
