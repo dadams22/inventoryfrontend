e8f15a579b48e5ab729785ba9290ce9c2f3ffe7a
David Adams <dadams2212@gmail.com>
2020-06-02 14:10:53 -0400

    Add selector hook to fetch items from the redux store for the Inventory scene

--- src/scenes/Inventory/index.tsx
+++ src/scenes/Inventory/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import {ApplicationState, InventoryItem} from "../../services/types";
+import {useSelector} from "react-redux";
 
 function Inventory() {
+    const items = useSelector((state: ApplicationState) => state.items);
+
     return <h1>Inventory</h1>;
 }
 
