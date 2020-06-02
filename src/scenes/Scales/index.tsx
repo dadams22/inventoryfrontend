aeb9d0eeba54eecc2bbad62e63fec8c2bd406fc1
David Adams <dadams2212@gmail.com>
2020-06-02 17:27:48 -0400

    Add sortable table with scale id and availability to the Scales scene

--- src/scenes/Scales/index.tsx
+++ src/scenes/Scales/index.tsx
@@ -1,9 +1,32 @@
 import React, {useState} from 'react'
-import {Col, Row} from "antd";
+import {Col, Row, Table, Tag} from "antd";
 import SearchBar from "../../components/SearchBar";
+import {useSelector} from "react-redux";
+import {ApplicationState, Scale} from "../../services/types";
 
 function Scales() {
+    const scales = useSelector((state: ApplicationState) => state.scales);
     const [searchValue, setSearchValue] = useState('');
+
+    const dataSource = scales
+        .filter(scale => String(scale.id).includes(searchValue.toLowerCase()))
+        .map(scale => ({ ...scale, key: scale.id }));
+
+    const columns = [
+        {
+            title: 'Scale ID',
+            dataIndex: 'id',
+            key: 'id',
+            sorter: (a: Scale, b: Scale) => a.id - b.id,
+        },
+        {
+            title: 'Availability',
+            dataIndex: 'inUse',
+            key: 'inUse',
+            sorter: (a: Scale, b: Scale) => Number(a.inUse) - Number(b.inUse),
+            render: (inUse: boolean) => inUse ? <Tag>In Use</Tag> : <Tag color={'green'}>Available</Tag>,
+        },
+    ];
 
     return (
         <>
@@ -18,7 +41,7 @@ function Scales() {
             </Row>
             <Row justify={'center'}>
                 <Col span={18}>
-
+                    <Table dataSource={dataSource} columns={columns}/>
                 </Col>
             </Row>
         </>
