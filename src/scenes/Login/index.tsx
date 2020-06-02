09242c2e222ea8c5f9034366074fc85c11b2ad2a
David Adams <dadams2212@gmail.com>
2020-06-02 18:39:20 -0400

    Fix styling of all code in src/ using prettier

--- src/scenes/Login/index.tsx
+++ src/scenes/Login/index.tsx
@@ -2,59 +2,58 @@ import React from 'react';
 import { Form, Input, Button, Row, Col } from 'antd';
 
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
 };
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+  wrapperCol: { offset: 8, span: 16 },
 };
 
 function Login() {
+  // TODO: - I have these result types as Object because I don't want to do 'any', but this feels wrong
+  const onFinish = (values: Object) => {
+    console.log('Success:', values);
+  };
 
-    // TODO: - I have these result types as Object because I don't want to do 'any', but this feels wrong
-    const onFinish = (values: Object) => {
-        console.log('Success:', values);
-    };
+  const onFinishFailed = (errorInfo: Object) => {
+    console.log('Failed:', errorInfo);
+  };
 
-    const onFinishFailed = (errorInfo: Object) => {
-        console.log('Failed:', errorInfo);
-    };
+  return (
+    <Row align={'middle'} justify={'center'} style={{ minHeight: '100vh' }}>
+      <Col span={6}>
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please enter your email.' }]}
+          >
+            <Input />
+          </Form.Item>
 
-    return (
-        <Row align={'middle'} justify={'center'} style={{ minHeight: '100vh' }}>
-            <Col span={6}>
-                <Form
-                    {...layout}
-                    name="basic"
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                >
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[{ required: true, message: 'Please enter your email.' }]}
-                    >
-                        <Input/>
-                    </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please enter your password.' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please enter your password.' }]}
-                    >
-                        <Input.Password/>
-                    </Form.Item>
-
-                    <Form.Item {...tailLayout}>
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Col>
-        </Row>
-    );
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
 }
 
 export default Login;
