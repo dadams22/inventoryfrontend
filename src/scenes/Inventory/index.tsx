2bb7e86087dc40ff1b143acb20d08d7678da0881
David Adams <dadams2212@gmail.com>
2020-06-02 14:28:26 -0400

    Add table containing inventory items to the Inventory scene with centered layout

--- src/scenes/Inventory/index.tsx
+++ src/scenes/Inventory/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import {ApplicationState, InventoryItem} from "../../services/types";
+import {ApplicationState} from "../../services/types";
 import {useSelector} from "react-redux";
+import {Col, Row, Table} from "antd";
 
 function Inventory() {
     const items = useSelector((state: ApplicationState) => state.items);
 
-    return <h1>Inventory</h1>;
+    const dataSource = items.map(item => ({...item, key: item.id}));
+    const columns = [
+        { title: 'Name', dataIndex:'name', key: 'name', },
+        { title: 'Weight', dataIndex: 'weight', key: 'weight', },
+    ];
+
+    return (
+        <Row justify={'center'}>
+            <Col span={18}>
+                <Table dataSource={dataSource} columns={columns} />
+            </Col>
+        </Row>
+    );
 }
 
 export default Inventory;
