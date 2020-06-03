a353afd308c06896ce43eec5280f467b65bfeacd
David Adams <dadams2212@gmail.com>
2020-06-03 11:46:25 -0400

    Remove unneeded index.css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
 import { store } from './store';
 
 ReactDOM.render(
