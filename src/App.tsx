5bd2bb2cf68d19ec1ea8cd4d62419357cb59713c
David Adams <dadams2212@gmail.com>
2020-06-03 12:43:15 -0400

    Use App.css in App.tsx, was not being used before

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,20 @@ const { Content } = Layout;
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <NavigationBar />
-        <Content>
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route path='/inventory' component={Inventory} />
-            <Route path='/scales' component={Scales} />
-          </Switch>
-        </Content>
-      </Layout>
-    </Router>
+    <div className='App'>
+      <Router>
+        <Layout>
+          <NavigationBar />
+          <Content>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route path='/inventory' component={Inventory} />
+              <Route path='/scales' component={Scales} />
+            </Switch>
+          </Content>
+        </Layout>
+      </Router>
+    </div>
   );
 }
 
