0b716ae63c89ef7ed2736a6e5aefba7a6cc7a938
David Adams <dadams2212@gmail.com>
2020-06-15 20:06:38 -0400

    Add working createItem method to the api wrapper

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
 class ApiWrapper {
-  API_BASE_URL = 'http://127.0.0.1:8000/api';
+  API_BASE_URL = 'http://127.0.0.1:8000/api/';
+
+  ENDPOINTS = {
+    getItems: 'items',
+    createItem: 'items/',
+    scales: 'scales',
+    tokenAuth: 'token-auth',
+  };
 
   instance: ReturnType<typeof axios.create>;
 
@@ -31,18 +38,33 @@ class ApiWrapper {
   }
 
   async login(payload: { username: string; password: string }) {
-    const response = await this.instance.post('/token-auth', payload);
+    const response = await this.instance.post(
+      this.ENDPOINTS.tokenAuth,
+      payload,
+    );
     const { token } = response.data;
     this.setTokenAuth(token);
   }
 
   async fetchScales() {
-    const response = await this.instance.get('/scales');
+    const response = await this.instance.get(this.ENDPOINTS.scales);
     return response.data;
   }
 
   async fetchItems() {
-    const response = await this.instance.get('/items');
+    const response = await this.instance.get(this.ENDPOINTS.getItems);
+    return response.data;
+  }
+
+  async createItem(payload: {
+    name: string;
+    description: string;
+    scales: number[];
+  }) {
+    const response = await this.instance.post(
+      this.ENDPOINTS.createItem,
+      payload,
+    );
     return response.data;
   }
 }
