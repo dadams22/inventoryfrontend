813f7b845c2c67525e5e74f0036168953f0a5d2f
David Adams <dadams2212@gmail.com>
2020-07-07 15:11:39 -0400

    Fix layout of the ItemDetails page and add description items for 'Date Stocked' and 'Linked Scales'

--- src/scenes/Home/components/Inventory/components/ItemDetails/index.tsx
+++ src/scenes/Home/components/Inventory/components/ItemDetails/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouteComponentProps, useParams, withRouter } from 'react-router-dom';
-import { Descriptions, Empty, PageHeader } from 'antd';
+import { Col, Descriptions, Empty, PageHeader, Row, Tag } from 'antd';
 import { useSelector } from 'react-redux';
 import { itemsSelectors } from '../../../../../../services/selectors';
 import { ApplicationState } from '../../../../../../store';
@@ -19,20 +19,32 @@ const ItemDetails = ({ history }: RouteComponentProps) => {
   }
 
   return (
-    <PageHeader
-      title={item.name}
-      subTitle='Item Details'
-      onBack={() => history.push('/inventory')}
-      ghost={false}
-    >
-      <Descriptions>
-        {item.description && (
-          <Descriptions.Item label='Description'>
-            {item.description}
-          </Descriptions.Item>
-        )}
-      </Descriptions>
-    </PageHeader>
+    <Row justify='center'>
+      <Col span={18}>
+        <PageHeader
+          title={item.name}
+          subTitle='Item Details'
+          onBack={() => history.push('/inventory')}
+          ghost={false}
+        >
+          <Descriptions>
+            {item.description && (
+              <Descriptions.Item label='Description'>
+                {item.description}
+              </Descriptions.Item>
+            )}
+            <Descriptions.Item label='Date Stocked'>
+              {item.created_at}
+            </Descriptions.Item>
+            <Descriptions.Item label='Linked Scales'>
+              {item.scales.map((scale) => (
+                <Tag>{scale}</Tag>
+              ))}
+            </Descriptions.Item>
+          </Descriptions>
+        </PageHeader>
+      </Col>
+    </Row>
   );
 };
 
